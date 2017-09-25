ac5e100edcef9755b6ce3827c8d7198eee74b5a7
Parul Gupta <parul.uit@gmail.com>
2017-09-25 10:28:29 +0530

    changed port

--- finalproject.py
+++ finalproject.py
@@ -198,6 +198,6 @@ def deletePlace(city_name,place_name):
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
 	app.debug = True
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=5000)
 
     
